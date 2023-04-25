3a1b8d9c8f553acd8f7a369c778b306329672b17
joitz01 <romeov007@gmail.com>
2023-04-25 11:45:44 -0600

    Update RankLevelHandler.cs

--- RankLevelHandler.cs
+++ RankLevelHandler.cs
@@ -106,82 +106,83 @@ namespace Server
 					db.ToLevel = (int)(db.Level * db.L2to20Multipier);
 					db.TotalSkillPoints += db.Below20;
 				}
-				if (db.Level <= 40)
+				else if (db.Level <= 40 && db.Level > 21)
 				{
 					db.ToLevel = (int)(db.Level * db.L21to40Multiplier);
 					db.TotalSkillPoints += db.Below40;
 				}
-				if (db.Level <= 60)
+				else if (db.Level <= 60 && db.Level > 41)
 				{
 					db.ToLevel = (int)(db.Level * db.L41to60Multiplier);
 					db.TotalSkillPoints += db.Below60;
 				}
-				if (db.Level <= 70)
+				else if (db.Level <= 70 && db.Level > 61)
 				{
 					db.ToLevel = (int)(db.Level * db.L61to70Multiplier);
 					db.TotalSkillPoints += db.Below70;
 				}
-				if (db.Level <= 80)
+				
+				else if (db.Level <= 80 && db.Level > 71)
 				{
 					db.ToLevel = (int)(db.Level * db.L71to80Multiplier);
 					db.TotalSkillPoints += db.Below80;
 				}
-				if (db.Level <= 90)
+				else if (db.Level <= 90 && db.Level > 81)
 				{
 					db.ToLevel = (int)(db.Level * db.L81to90Multipier);
 					db.TotalSkillPoints += db.Below90;
 				}
-				if (db.Level <= 100)
+				else if (db.Level <= 100 && db.Level > 91)
 				{
 					db.ToLevel = (int)(db.Level * db.L91to100Multipier);
 					db.TotalSkillPoints += db.Below100;
 				}
-				if (db.Level <= 110)
+				else if (db.Level <= 110 && db.Level > 101)
 				{
 					db.ToLevel = (int)(db.Level * db.L101to110Multiplier);
 					db.TotalSkillPoints += db.Below110;
 				}
-				if (db.Level <= 120)
+				else if (db.Level <= 120 && db.Level > 111)
 				{
 					db.ToLevel = (int)(db.Level * db.L111to120Multiplier);
 					db.TotalSkillPoints += db.Below120;
 				}
-				if (db.Level <= 130)
+				else if (db.Level <= 130 && db.Level > 121)
 				{
 					db.ToLevel = (int)(db.Level * db.L121to130Multiplier);
 					db.TotalSkillPoints += db.Below130;
 				}
-				if (db.Level <= 140)
+				else if (db.Level <= 140 && db.Level > 131)
 				{
 					db.ToLevel = (int)(db.Level * db.L131to140Multiplier);
 					db.TotalSkillPoints += db.Below140;
 				}
-				if (db.Level <= 150)
+				else if (db.Level <= 150 && db.Level > 141)
 				{
 					db.ToLevel = (int)(db.Level * db.L141to150Multiplier);
 					db.TotalSkillPoints += db.Below150;
 				}
-				if (db.Level <= 160)
+				else if (db.Level <= 160 && db.Level > 151)
 				{
 					db.ToLevel = (int)(db.Level * db.L151to160Multiplier);
 					db.TotalSkillPoints += db.Below160;
 				}
-				if (db.Level <= 170)
+				else if (db.Level <= 170 && db.Level > 161)
 				{
 					db.ToLevel = (int)(db.Level * db.L161to170Multiplier);
 					db.TotalSkillPoints += db.Below170;
 				}
-				if (db.Level <= 180)
+				else if (db.Level <= 180 && db.Level > 171)
 				{
 					db.ToLevel = (int)(db.Level * db.L171to180Multiplier);
 					db.TotalSkillPoints += db.Below180;
 				}
-				if (db.Level <= 190)
+				else if (db.Level <= 190 && db.Level > 181)
 				{
 					db.ToLevel = (int)(db.Level * db.L181to190Multiplier);
 					db.TotalSkillPoints += db.Below190;
 				}
-				if (db.Level <= 200)
+				else if (db.Level <= 200 && db.Level > 191)
 				{
 					db.ToLevel = (int)(db.Level * db.L191to200Multiplier);
 					db.TotalSkillPoints += db.Below200;
